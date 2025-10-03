f3c45b1fb8b4577f34a4649cd1341cbe74d78841
Abhishek2000649 <ak7714832@gmail.com>
2025-10-03 12:37:26 +0530

    Login & Signup Pages

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/dashboard', function () {
     return view('dashboard');
 });
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/login', function () {
+    return view('auth.login');
+})->name('login');
 
+Route::get('/sign_up', function () {
+    return view('auth.sign_up');
+})->name('sign_up');
+Route::get('/forgetPassword', function () {
+    return view('auth.forgetPassword');
+})->name('forgetPassword');
